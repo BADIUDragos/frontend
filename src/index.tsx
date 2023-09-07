5efd041ba295169bda7d2d2cf4b7a8d18fe1e49a
BADIUDragos <dragos.badiu.a@hotmail.com>
2023-09-07 15:40:15 -0400

    think this fixes it

--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,14 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+root.render(
+  <React.StrictMode>
+      <Provider store={store}>
+              <App />
+      </Provider>
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
